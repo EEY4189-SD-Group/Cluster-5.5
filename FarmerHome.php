38421caf68050029d54dda136ad50c8817edb647
Hethuja .C <112101243+hethujaC@users.noreply.github.com>
2023-12-07 19:14:45 +0530

    Add files via upload

---
+++ FarmerHome.php
@@ -0,0 +1,123 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="Images/LOGO.png">
+    <title>AGRO</title>
+    <style><?php include "Css/Farmer.css" ?></style>
+    <style><?php include "Css/Footer.css" ?></style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Uchen&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+    
+</head>
+<body></
+<div class="content">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#" id="logo">
+            <img src="Images/LOGO.png" width="50px">
+            <span style="color:black">AG</span><span style="color:rgb(11, 212, 11)">RO</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="CustomerHome.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="About us.php">ABOUT US</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">CATEGORIES <i class="fas fa-chevron-down"></i></a>
+                <div class="dropdown-menu">
+                    <a href="Seeds.php" class="dropdown-item">Seeds</a>
+                    <a href="Vegetables.php" class="dropdown-item">Vegetables</a>
+                    <a href="Fruits.php" class="dropdown-item">Fruits</a>
+                </div>
+            </li>
+
+              <li class="nav-item">
+                  <a class="nav-link" href="Shop.php">SHOP</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="contact.php">CONTACT US</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="Blog.php">BLOG</a>
+              </li>
+                <!-- Your navigation items -->
+            </ul>
+        </div>
+        <div class="icons">
+            <a href="card.php"><img src="Images/add to.svg" width="20px"></a>
+            <a href="User.php"><img src="Images/userf.svg" width="24px"></a>
+            <a href="Selection.php"><img src="Images/lo.png" width="40px"></a>
+        </div>
+    </nav>
+</div>
+
+<!-- banner -->
+<section id="banner">
+    <img src="Images/HomeFarmer.png" class="bg-1" alt="bg">
+</section>
+
+<!-- Text between banner and buttons -->
+<div class="text-between">
+    <div class="delivery-info">
+        <span class="running-text">Grow your own business here!</span>
+    </div>
+</div>
+<div id="product-title">Select Your Product</div>
+<section id="images-section">
+    <!-- Image 1 with a link -->
+    <a href="AddProduct.php">
+        <img src="Images/add.jpg" alt="Image 1" class="responsive-image">
+    </a>
+    <!-- Image 2 with a link -->
+    <a href="OrderView.php">
+        <img src="Images/order.jpg" alt="Image 2" class="responsive-image">
+    </a>
+
+    <!-- Image 3 with a link -->
+    <a href="Shop.php">
+        <img src="Images/Shop.jpg" alt="Image 3" class="responsive-image">
+    </a>
+</section>
+<section id="text-section">
+     <div class="responsive-text" style="margin-left: 30px;"><a href="#">Add Products</a></div>
+      <div class="responsive-text" style="margin-left: 10px;"><a href="OrderView.php">Orders</a></div>
+       <div class="responsive-text" style="margin-left: 50px;"><a href="Shop.php">Shop</a></div>
+</section>
+
+<footer>
+  <p class="text">Fresh Organic Vegetables and Fruits</p>
+
+  <ul class="list-footer">
+      <li><a href="FarmerHome.php">Home</a></li>
+      <li><a href="About us.php">About Us</a></li>
+      <li><a href="Vegetables.php">Categories</a></li>
+      <li><a href="Shop.php">Shop</a></li>
+      <li><a href="contact.php">Contact Us</a></li>
+      <li><a href="Blog.php">Blog</a></li>
+  </ul>
+  <div class="social-media">
+      <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+      <a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a>
+      <a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a>
+      <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+      <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+  </div>
+  <p class="copyrights">&copy; Agro</p>
+</footer>
+
+
+</body>
+</html>
---
+++ user.js
@@ -0,0 +1,7 @@
+// You can add JavaScript code here for additional functionality, such as form submission handling.
+
+document.getElementById('profile-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add your form submission logic here, for example, sending data to a server.
+    console.log('Form submitted!');
+});
